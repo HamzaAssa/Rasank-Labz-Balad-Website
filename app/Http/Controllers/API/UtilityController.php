214c1f1278eb58adccaec50f07307254538a12d7
Hamza <hamzaassarehman@gmail.com>
2024-11-14 11:09:33 +0500

    word downloading fixed

--- app/Http/Controllers/API/UtilityController.php
+++ app/Http/Controllers/API/UtilityController.php
@@ -17,23 +17,35 @@ class UtilityController extends Controller
     {
         $version = $request->query('version') + 1;
 
-        $oldestPublishinglog = PublishLog::where('version', $version)->first();
-        $lastPublishLog = PublishLog::latest('id')->first();
-
-        $words = Word::where('id', '>=', $oldestPublishinglog->start_id)->get();
-        $wordRelations = WordToWord::all();
-        $definitions = Definition::all();
-        $examples = Example::all();
+        $oldestPublishinglog = PublishLog::find($version);
 
         $response = [
-            "statusCode" => 200,
-            "body" => count($words)." new words downloaded.",
-            "words" => $words,
-            "definitions" => $definitions,
-            "examples" => $examples,
-            "wordRelations" => $wordRelations,
-            "newDBVersion" => $lastPublishLog->version,
+            "text" => "No new words available.",
+            "words" => [],
+            "definitions" => [],
+            "examples" => [],
+            "wordRelations" => [],
+            "newDBVersion" => $request->version,
         ];
+
+        if(!empty($oldestPublishinglog)) {
+            $lastPublishLog = PublishLog::latest('id')->first();
+
+            $words = Word::select('id', 'word', 'language')->where('id', '>=', $oldestPublishinglog->start_id)->where('status', 3)->get();
+            $wordRelations = WordToWord::select('id', 'balochi_id', 'urdu_id', 'english_id', "roman_balochi_id")->get();
+            $definitions = Definition::select('id', 'definition', 'word_id')->get();
+            $examples = Example::select('id', 'example', 'definition_id')->get();
+
+            $response = [
+                "text" => count($words)." new words downloaded.",
+                "words" => $words,
+                "definitions" => $definitions,
+                "examples" => $examples,
+                "wordRelations" => $wordRelations,
+                "newDBVersion" => $lastPublishLog->id,
+            ];
+        }
+
         return response()->json($response);
     }
 
@@ -43,26 +55,18 @@ class UtilityController extends Controller
         $words = $request->input('words');
         $wordRelations = $request->input('relations');
 
-        foreach ($words as $row) {
-            $word = new Word();
-            $word->word = $row->word;
-            $word->language = $row->language;
-            $word->status = 0;
-            $word->save();
-        }
-        foreach ($wordRelations as $row) {
-            $wordRelation = new WordToWord();
-            $wordRelation->balochi_id = $row->balochi_id;
-            $wordRelation->urdu_id = $row->urdu_id;
-            $wordRelation->english_id = $row->english_id;
-            $wordRelation->roman_balochi_id = $row->roman_balochi_id;
-            $wordRelation->save();
-        }
+        DB::transaction(function() use($request, &$words, &$wordRelations){
+            foreach ($words as $row) {
+                $word = new Word();
+                $word->word = $row["word"];
+                $word->language = $row["language"];
+                $word->status = 0;
+                $word->save();
+            }
+        });
 
-        $response = [
-            "statusCode" => 200,
-            "body" => count($words)." new words uploaded.",
-        ];
+        $response = count($words)." new words uploaded.";
+        
         return response()->json($response);
     }
 }
